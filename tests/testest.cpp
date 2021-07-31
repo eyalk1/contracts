afdef1a1b921c5516d20ccf03b7c3d0c63808658
Eyal Kamitchi <eyalkamitchi1@gmail.com>
2021-07-31 20:43:13 +0300

    first test is on the way

--- tests/testest.cpp
+++ tests/testest.cpp
@@ -3,7 +3,26 @@
 #include "../Throwing.hpp"
 #include <vector>
 
-TEST(HelloTest, BasicAssertions){
-    std::vector<int> v = {1,2,3,4,5,5,6,7};
-    
+TEST(HelloTest, BasicAssertions) {
+
+  std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8};
+  try {
+    auto c = TCONTRACT(pre<std::runtime_error> =
+                           Condata([&v] { return v.size() == 8; },
+                                   "vector should start with 8 integers"),
+                       post<std::logic_error> =
+                           Condata([&v] { return v.size() == 8; },
+                                   "vector size should get bigger."));
+    for (uint i = 0; i < 100; i++)
+      v.push_back(i);
+  } catch (...) {
+    std::cout << "heyo\n";
+  }
+
+  //   auto c = TCONTRACT(
+  //       pre<std::runtime_error> = Condata([&v] { return v.size() == 8; },
+  //                                         "vector should start with 8
+  //                                         integers"),
+  //       post<std::logic_error> = Condata([&v] { return v.size() > 8; },
+  //                                        "vector size should get bigger."));
 }
